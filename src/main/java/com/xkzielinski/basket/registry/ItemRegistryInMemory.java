ef49b5e59afbb244f016521acaf677b0b1d2acc1
kazi87 <xk.zielinski@gmail.com>
2016-02-22 00:10:44 +0100

    fix the orange price

--- src/main/java/com/xkzielinski/basket/registry/ItemRegistryInMemory.java
+++ src/main/java/com/xkzielinski/basket/registry/ItemRegistryInMemory.java
@@ -13,9 +13,9 @@ public class ItemRegistryInMemory implements ItemRegistry {
     private final Map<String, Item> registry = new HashMap() {
             {
                 put("apple", new Item("Apple", new BigDecimal(0.25), Item.DEFAULT_CURRENCY));
-                put("orange", new Item("Orange", new BigDecimal(30), Item.DEFAULT_CURRENCY));
+                put("orange", new Item("Orange", new BigDecimal(0.30), Item.DEFAULT_CURRENCY));
                 put("garlic", new Item("Garlic", new BigDecimal(0.15), Item.DEFAULT_CURRENCY));
-                put("papaya", new Item("Papaya", new BigDecimal(0.5), Item.DEFAULT_CURRENCY));
+                put("papaya", new Item("Papaya", new BigDecimal(0.50), Item.DEFAULT_CURRENCY));
 
             }
         };
